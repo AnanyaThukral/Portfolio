cd7f9d7fb872c2b3ec96477aa83cbef7f3769d4e
Ananya <ananya@uoguelph.ca>
2021-08-28 03:51:47 -0400

    about me created yet to be routed

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const About = () => {
+    return (
+        <Container>
+            About
+        </Container>
+    )
+}
+
+export default About
+
+const Container = styled.div``
