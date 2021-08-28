9bb4022ebd3b70397a9371d48034ef4e8c8edd91
Ananya <ananya@uoguelph.ca>
2021-08-28 19:35:46 -0400

    changing data grab to firebase

---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC75I_m_bKzCDvkg1I3baFtrNFspggqJWM",
+    authDomain: "portfolio-2fa97.firebaseapp.com",
+    projectId: "portfolio-2fa97",
+    storageBucket: "portfolio-2fa97.appspot.com",
+    messagingSenderId: "778878770136",
+    appId: "1:778878770136:web:d55404a7157e1adf9270ad",
+    measurementId: "G-F17YHKL1RF"
+  };
+
+  //initialize the firebase app
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+  //initialize the database
+  const db = firebaseApp.firestore();
+
+  export default db
