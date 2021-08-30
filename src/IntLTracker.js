a2b32f22e9a759e7fefc6e2b93e981d47ec85af6
Ananya <ananya@uoguelph.ca>
2021-08-29 23:39:11 -0400

    routed to project page

---
+++ src/IntLTracker.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const IntLTracker = () => {
+    return (
+        <Container>
+            intL Tracker
+        </Container>
+    )
+}
+
+export default IntLTracker
+
+const Container = styled.div``
