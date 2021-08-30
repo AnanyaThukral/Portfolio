7857947f32ff6255adb167a78626f45c58c401e2
Ananya <ananya@uoguelph.ca>
2021-08-29 22:53:09 -0400

    Skills added

---
+++ src/TagItem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TagItem = ({tag}) => {
+    return (
+        <Container>
+            {tag}
+        </Container>
+    )
+}
+
+export default TagItem
+
+const Container = styled.div`
+    border-radius: 5px;
+    padding: 5px 8px; 
+    color: rgba(255, 255, 255, 0.6);
+    font-size: 14px;
+    background-color: transparent;
+    border: 0.3px solid; 
+    :hover{
+        box-shadow: 0 0.5em 0.5em -0.4em var(#e5ff60);
+        transform: translateY(-0.25em);
+    }
+    :focus{
+        box-shadow: 0 0.5em 0.5em -0.4em var(#e5ff60);
+        transform: translateY(-0.25em);
+    }
+`
