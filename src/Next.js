63d4901fecc79b6b660998d9784fb867aeacc296
Ananya <ananya@uoguelph.ca>
2021-09-08 03:18:10 -0400

    project next step section

---
+++ src/Next.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Next = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Next
+
+const Container = styled.div``
