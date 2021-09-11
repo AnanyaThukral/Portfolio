14aa2dc159cd9b1ec9b0e211857d4e934e7ee0d0
Ananya <ananya@uoguelph.ca>
2021-09-11 11:40:06 -0400

    new web clones project

---
+++ src/WebClones.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const WebClones = () => {
+    return (
+        <Container>
+           WebClones
+        </Container>
+    )
+}
+
+export default WebClones
+
+const Container = styled.div``
