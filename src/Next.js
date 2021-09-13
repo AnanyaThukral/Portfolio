204f078e6217e9df3355ef8cef511e51f266a803
Ananya <ananya@uoguelph.ca>
2021-09-13 01:42:02 -0400

    fixed width

--- src/Next.js
+++ src/Next.js
@@ -6,7 +6,7 @@ const Next = () => {
         <Container>
             <span>what's next?</span>
             <TextContainer>
-                <Text>The vision of this idea is to have a <Bold>Advisor Dashboard</Bold> as well along with a <Bold>Student Dashboard</Bold>. The reason being - there was a concern raised by the international advisors while sharing the idea with them that as we are dealing with immigration that could potentially result in sharing lot of confidential information between students.</Text> 
+                <Text>The vision of this idea is to have a <Bold>Advisor Dashboard</Bold> as well along with a <Bold>Student Dashboard</Bold>. The reason being that a concern raised by the international advisors while sharing the idea with them that as we are dealing with immigration that could potentially result in sharing lot of confidential information between students.</Text> 
                 <Text>Therefore, I also want to include an Advisor dashboard which can give Advisors administrative rights. Further, I would like to <Bold>explore more ways to make this application secure.</Bold> Additionally, Advisors can have a broader view about the goals of students and their needs - based on that they can organize more events and workshops specific to those topics. </Text>
                 <Text>Next milestone for me is <Bold>Usability Testing</Bold> with the initial Prototype. As this project and idea has been approved by the school of computer science at Guelph. I will be doing usability testing under the supervision of HCI faculty in F21 semester.</Text>
                 <Text>After usability Testing and reflections on the design - I will be developing the <Bold>frontend of the website with latest, fast, scalable and simple Javascript framework - React JS.</Bold></Text>
@@ -24,6 +24,9 @@ const Container = styled.div`
     color: #14213D;
     span{
         font-size: 1.7rem;
+    }
+    @media (max-width: 1300px){
+        min-width: 900px;
     }
 `
 
